a310d6d4898328517075867c9c58e8628155f407
IYgit <novemb75@gmail.com>
2024-05-12 16:41:53 +0300

    task 1

---
+++ task_1.py
@@ -0,0 +1,25 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+# Створення графа
+G = nx.Graph()
+
+# Додавання вершин (перехрестя)
+G.add_nodes_from([1, 2, 3, 4, 5])
+
+# Додавання ребер (вулиць)
+edges = [(1, 2), (2, 3), (3, 4), (4, 5), (5, 1), (1, 3), (2, 4), (3, 5)]
+G.add_edges_from(edges)
+
+# Візуалізація графа
+plt.figure(figsize=(8, 6))
+nx.draw(G, with_labels=True, node_size=1000, node_color='lightblue', font_size=12, font_weight='bold')
+plt.title("Мережа вулиць міста")
+plt.show()
+
+# Аналіз характеристик графа
+print("Кількість вершин:", G.number_of_nodes())
+print("Кількість ребер:", G.number_of_edges())
+print("Список вершин:", list(G.nodes()))
+print("Список ребер:", list(G.edges()))
+print("Ступінь вершин:", dict(G.degree()))
